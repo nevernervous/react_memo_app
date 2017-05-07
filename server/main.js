c22eebbf27ce4d24026ecc49ec93942f552e994e
nevernervous <brndncostoya@gmail.com>
2017-05-08 01:55:24 +0300

    added webpack.config file.

---
+++ server/main.js
@@ -0,0 +1,57 @@
+/**
+ * Created by denis on 5/8/17.
+ */
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+
+const devPort = 4000;
+
+import express from 'express';
+import path from 'path';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+    console.log('Conneted to mongodb server');
+    mongoose.connect('mongodb://localhost/codelab');
+});
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use('/', express.static(path.join(__dirname, './../public')));
+
+app.get('/hello', (req, res) => {
+    return res.send('Hello CodeLab');
+});
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port);
+});
+
+if (process.env.NODE_ENV == 'development') {
+    console.log('Server is running on development mode.');
+    const config = require('../webpack.dev.config');
+    const compiler = webpack(config);
+    const devServer = new WebpackDevServer(compiler, config.devServer);
+    devServer.listen(
+        devPort, () => {
+            console.log('webpack-dev-server is listenging on port', devPort);
+        }
+    );
+}
---
+++ server/routes/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by denis on 5/8/17.
+ */
